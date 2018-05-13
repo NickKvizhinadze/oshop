c250e6b5c1df17c091b369ebb448e3d1a8f21530
NickKvizhinadze <nickkvizhinadze@gmail.com>
2018-05-13 21:30:48 +0400

    Added auth guard to protect routes and return url while logging

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,20 @@
+import { AuthService } from './auth.service';
+import { Injectable } from '@angular/core';
+import { Router, CanActivate, RouterStateSnapshot } from '@angular/router';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router, private auth: AuthService) { }
+
+  canActivate(route, state: RouterStateSnapshot) {
+    return this.auth.user$.map(user => {
+      if (user) {
+        return true;
+      }
+      this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
+      return false;
+    });
+  }
+}
