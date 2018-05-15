af8d56ba71f90c69cca3f7fceaf51cc44fa088c6
NickKvizhinadze <nickkvizhinadze@gmail.com>
2018-05-15 17:44:41 +0400

    Added datatable grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular5-data-table';
 
 import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
@@ -48,6 +49,7 @@ import { ProductService } from './product.service';
     BrowserModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
